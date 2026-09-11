34c22b29b0f7c771e11f3638fdfd0c5e7477954f
Erick <erickmonlau@gmail.com>
2026-09-11 15:47:41 +0200

    fix(hero): use min-h-0 on mobile hero so Marquee Ribbon is naturally in initial screen view

--- src/hero-carousel.tsx
+++ src/hero-carousel.tsx
@@ -70,10 +70,10 @@ export default function HeroCarousel({
   return (
     <section
       id="hero"
-      className={`relative text-slate-900 min-h-[100vh] sm:min-h-screen ${
+      className={`relative text-slate-900 min-h-0 sm:min-h-screen ${
         customHeadline
-          ? "pt-20 xs:pt-22 sm:pt-28 lg:pt-32"
-          : "pt-20 xs:pt-22 sm:pt-28 lg:pt-32"
+          ? "pt-16 sm:pt-28 lg:pt-32"
+          : "pt-16 sm:pt-28 lg:pt-32"
       } pb-0 flex flex-col justify-between overflow-hidden select-none bg-[#F8FAFC] px-4 md:px-8 xl:px-12`}
     >
       <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden">

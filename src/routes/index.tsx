573b7870d5fb891f844431736175b17286bce59e
Erick <erickmonlau@gmail.com>
2026-09-19 00:58:16 +0200

    fix: restore FooterMascot lazy import in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,6 +15,7 @@ import gesgramaOffice from "@/assets/gesgrama_storefront_final.webp";
 
 const WhatsAppButton = lazy(() => import('@/components/WhatsAppButton').then(m => ({ default: m.WhatsAppButton })));
 const CookieBanner = lazy(() => import('@/components/CookieBanner'));
+const FooterMascot = lazy(() => import('@/components/FooterMascot').then(m => ({ default: m.FooterMascot })));
 
 
 export const Route = createFileRoute("/")({
